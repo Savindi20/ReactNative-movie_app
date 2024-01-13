9185eb550be026e54a2cfeadfd83f09cb0701353
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-14 00:30:21 +0530

    home screen update with bbackground

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import { Bars3CenterLeftIcon } from 'react-native-heroicons/outline';
 
+const ios = Platform.OS == 'ios';
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View className="bg-neutral-800">
+      {/* Search Bar and logo */}
+      <SafeAreaView className={ios?"-mb-2": 'mb-3'}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+        </View>
+      </SafeAreaView>
+    </View>
   )
 }
