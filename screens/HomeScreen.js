0a173003dbe5c675535338fcc5628b4f2600a4af
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-14 11:03:10 +0530

    home screen and theme index file updated

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,8 +13,21 @@ export default function HomeScreen() {
         <StatusBar style="light" />
         <View className="flex-row justify-between items-center mx-4">
           <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text 
+            className="text-white text-3xl font-bold">
+              <Text style={styles.text}>M</Text>ovies
+          </Text>
+          <TouchableOpacity onPress={()=> navigation.navigate('Search')}>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
+
+      <ScrollView 
+        showsVerticalScrollIndicator={false} 
+        contentContainerStyle={{paddingBottom: 10}}
+      >
+      </ScrollView>
     </View>
   )
 }
