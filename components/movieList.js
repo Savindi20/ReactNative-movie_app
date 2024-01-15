8468218ae0a04f8bbd25e5ebcb874c77bcc6ff09
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-15 14:53:44 +0530

    images and icons added

--- components/movieList.js
+++ components/movieList.js
@@ -1,5 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { styles } from '../theme';
+const {width, height} =  Dimensions.get('window');
 
 export default function MovieList({title, hideSeeAll, data}) {
   const navigation = useNavigation();
