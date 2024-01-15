d7a80ef224c2dfe56b9d5434cc04a2fe3c1dc85a
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-15 14:47:47 +0530

    movie list component updated

--- components/movieList.js
+++ components/movieList.js
@@ -1,10 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function MovieList() {
+export default function MovieList({title, hideSeeAll, data}) {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>MovieList</Text>
+    <View className="mb-8 space-y-4">
+      
+      <View className="mx-4 flex-row justify-between items-center">
+        <Text className="text-white text-lg">{title}</Text>
+        {
+          !hideSeeAll && (
+            <TouchableOpacity>
+              <Text style={styles.text} className="text-lg">See All</Text>
+            </TouchableOpacity>
+          )
+        }
+        
+        
+      </View>
     </View>
   )
 }
