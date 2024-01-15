dcaa87dfd7269ff977cb0d451be845a03156fb88
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-15 23:32:36 +0530

    home screen updated getTrendingMovies getUpcomingMovies functions added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,55 @@
-import { View, Text, Platform } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { StatusBar } from 'expo-status-bar'
-import { Bars3CenterLeftIcon } from 'react-native-heroicons/outline';
+import { View, Text, TouchableOpacity, ScrollView, Platform } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+import TrendingMovies from '../components/trendingMovies';
+import MovieList from '../components/movieList';
+import { StatusBar } from 'expo-status-bar';
+import { fetchTopRatedMovies, fetchTrendingMovies, fetchUpcomingMovies } from '../api/moviedb';
+import { useNavigation } from '@react-navigation/native';
+import Loading from '../components/loading';
+import { styles } from '../theme';
 
-const ios = Platform.OS == 'ios';
+const ios = Platform.OS === 'ios';
+
 export default function HomeScreen() {
 
-  const [trending, setTrending] = useState([1,2,3]);
-  const [upcoming, setUpcoming] = useState([1,2,3]);
-  const [topRated, setTopRated] = useState([1,2,3]);
+  const [trending, setTrending] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
+
+
+  useEffect(()=>{
+    getTrendingMovies();
+    getUpcomingMovies();
+    getTopRatedMovies();
+  },[]);
+
+  const getTrendingMovies = async ()=>{
+    const data = await fetchTrendingMovies();
+    console.log('got trending', data.results.length)
+    if(data && data.results) setTrending(data.results);
+    setLoading(false)
+  }
+  const getUpcomingMovies = async ()=>{
+    const data = await fetchUpcomingMovies();
+    console.log('got upcoming', data.results.length)
+    if(data && data.results) setUpcoming(data.results);
+  }
+  const getTopRatedMovies = async ()=>{
+    const data = await fetchTopRatedMovies();
+    console.log('got top rated', data.results.length)
+    if(data && data.results) setTopRated(data.results);
+  }
+
+
 
   return (
-    <View className="bg-neutral-800">
-      {/* Search Bar and logo */}
-      <SafeAreaView className={ios?"-mb-2": 'mb-3'}>
+    <View className="flex-1 bg-neutral-800">
+      {/* search bar */}
+      <SafeAreaView className={ios? "-mb-2": "mb-3"}>
         <StatusBar style="light" />
         <View className="flex-row justify-between items-center mx-4">
           <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
@@ -27,23 +62,27 @@ export default function HomeScreen() {
           </TouchableOpacity>
         </View>
       </SafeAreaView>
-
-      <ScrollView 
+      {
+        loading? (
+          <Loading />
+        ):(
+          <ScrollView 
             showsVerticalScrollIndicator={false} 
             contentContainerStyle={{paddingBottom: 10}}
           >
 
             {/* Trending Movies Carousel */}
-            <TrendingMovies data={trending} />
+            { trending.length>0 && <TrendingMovies data={trending} /> }
 
             {/* upcoming movies row */}
-            <MovieList title="Upcoming" data={upcoming} />
+            { upcoming.length>0 && <MovieList title="Upcoming" data={upcoming} /> }
             
-
             {/* top rated movies row */}
-            <MovieList title="Top Rated" data={topRated} />
+            { topRated.length>0 && <MovieList title="Top Rated" data={topRated} /> }
 
           </ScrollView>
-    </View>
+        )
+      }
+  </View>      
   )
 }
