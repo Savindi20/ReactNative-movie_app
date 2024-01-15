7380059064f63ba00216ff196540e644369e476b
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-15 19:31:27 +0530

    movie screen updated with movie details

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,25 +1,21 @@
-import {
-  View,
-  Text,
-  Dimensions,
-  TouchableOpacity,
-  ScrollView,
-  Platform,
-} from "react-native";
-import React, { useEffect } from "react";
-import { useRoute } from "@react-navigation/native";
-import { ChevronLeftIcon } from "react-native-heroicons/outline";
-import { HeartIcon } from "react-native-heroicons/solid";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { styles, theme } from "../theme";
+import { View, Text, Image, Dimensions, TouchableOpacity, ScrollView, Platform } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { LinearGradient } from 'expo-linear-gradient'
+import { ChevronLeftIcon } from 'react-native-heroicons/outline';
+import { HeartIcon} from 'react-native-heroicons/solid';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { fallbackMoviePoster, image500 } from '../api/moviedb';
+import { styles, theme } from '../theme';
 
-const ios = Platform.OS == "ios";
-const topMargin = ios ? "" : " mt-3";
-var { width, height } = Dimensions.get("window");
+const ios = Platform.OS == 'ios';
+const topMargin = ios? '':' mt-3';
+var {width, height} = Dimensions.get('window');
 
 export default function MovieScreen() {
-  const { params: item } = useRoute();
+  const {params: item} = useRoute();
   const navigation = useNavigation();
+  const [movie] = useState({});
   const [isFavourite, toggleFavourite] = useState(false);
 
   useEffect(() => {
@@ -71,6 +67,47 @@ export default function MovieScreen() {
                     end={{ x: 0.5, y: 1 }}
                     className="absolute bottom-0"
                 />
+            </View>
+
+            {/* movie details */}
+            <View style={{marginTop: -(height*0.09)}} className="space-y-3">
+                {/* title */}
+                <Text className="text-white text-center text-3xl font-bold tracking-widest">
+                {
+                    movie?.title
+                }
+                </Text>
+
+                {/* status, release year, runtime */}
+                {
+                    movie?.id? (
+                        <Text className="text-neutral-400 font-semibold text-base text-center">
+                            {movie?.status} • {movie?.release_date?.split('-')[0] || 'N/A'} • {movie?.runtime} min
+                        </Text>
+                    ):null
+                }
+        
+                {/* genres  */}
+                <View className="flex-row justify-center mx-4 space-x-2">
+                    {
+                        movie?.genres?.map((genre,index)=>{
+                            let showDot = index+1 != movie.genres.length;
+                            return (
+                                <Text key={index} className="text-neutral-400 font-semibold text-base text-center">
+                                    {genre?.name} {showDot? "•":null}
+                                </Text>
+                            )
+                        })
+                    }
+                </View>
+
+                {/* description */}
+                <Text className="text-neutral-400 mx-4 tracking-wide">
+                    {
+                        movie?.overview
+                    }
+                </Text>
+        
             </View>
         </View>
     </ScrollView>
