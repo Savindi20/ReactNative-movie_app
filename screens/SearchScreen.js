b8ce98d975bc49dc71f7930aaa09b6a107842a03
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-16 19:27:33 +0530

    search screen search results part added

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity, Dimensions } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image, ScrollView, TouchableWithoutFeedback, Dimensions } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { XMarkIcon } from 'react-native-heroicons/outline'
@@ -27,6 +27,55 @@ export default function SearchScreen() {
                 
             </TouchableOpacity>
         </View>
+
+        {/* search results */}
+        {
+            loading? (
+                <Loading />
+            ): 
+            results.length>0? (
+                <ScrollView 
+                    showsVerticalScrollIndicator={false} 
+                    contentContainerStyle={{paddingHorizontal:15}}
+                    className="space-y-3"
+                >
+                    <Text className="text-white font-semibold ml-1">Results ({results.length})</Text>
+                    <View className="flex-row justify-between flex-wrap">
+                        {
+                            results.map((item, index)=>{
+                                return (
+                                    <TouchableWithoutFeedback 
+                                        key={index} 
+                                        onPress={()=> navigation.push('Movie', item)}>
+                                        <View className="space-y-2 mb-4">
+                                            <Image 
+                                                source={{uri: image185(item.poster_path) || fallbackMoviePoster}} 
+                                                // source={require('../assets/images/moviePoster2.png')}
+                                                className="rounded-3xl" 
+                                                style={{ width: width*0.44, height: height*0.3}} 
+                                            />
+                                            <Text className="text-gray-300 ml-1">
+                                                {
+                                                    item.title.length>22? item.title.slice(0,22)+'...': item.title
+                                                }
+                                            </Text>
+                                        </View>
+                                    </TouchableWithoutFeedback>
+                                )
+                            })
+                        }
+                    </View>
+                    
+                </ScrollView>
+            ):(
+                <View className="flex-row justify-center">
+                    <Image 
+                        source={require('../assets/images/movieTime.png')} 
+                        className="h-96 w-96"
+                    />
+                </View>
+            )
+        }
     </SafeAreaView>
   )
 }
