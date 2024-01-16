9fb6d0c4cb36e39ed79e2d7496623d5e5c73d36e
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2024-01-16 16:08:02 +0530

    search screen updated with basic styles

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,6 +1,8 @@
-import { View, Dimensions } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Dimensions } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { XMarkIcon } from 'react-native-heroicons/outline'
+
 const {width, height} =  Dimensions.get('window');
 
 
@@ -11,6 +13,19 @@ export default function SearchScreen() {
         {/* search input */}
         <View 
             className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full" >
+            <TextInput 
+                onChangeText={handleTextDebounce} 
+                placeholder="Search Movie" 
+                placeholderTextColor={'lightgray'} 
+                className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider" 
+            />
+            <TouchableOpacity 
+                onPress={()=> navigation.navigate('Home')}
+                className="rounded-full p-3 m-1 bg-neutral-500" 
+            >
+                <XMarkIcon size="25" color="white" />
+                
+            </TouchableOpacity>
         </View>
     </SafeAreaView>
   )
